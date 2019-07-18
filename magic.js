f9ffa732d8ee1754e2a03143379ac9f86d123eb9
Jesse McKinney <jessman51386@gmail.com>
2019-07-18 16:32:25 -0400

    yay

--- magic.js
+++ magic.js
@@ -1,40 +1,22 @@
-function initialize() {
-    var mapOptions = {
-      zoom: 18,
-      center: new google.maps.LatLng(33.9378604, -84.4770031),
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-    var map = new google.maps.Map(document.getElementById('map-canvas'),
-                                  mapOptions);
-    // Try HTML5 geolocation
-    if(navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = new google.maps.LatLng(position.coords.latitude,
-                                         position.coords.longitude);
-  
-      var image = 'http://www.battleoftheboyne.ie/media/Bus.png';
-        var busMarker = new google.maps.Marker({
-            position: pos,
-            map: map,
-            icon: image
-        });
-  
-        map.setCenter(pos);
-      }, function() {
-        handleNoGeolocation(true);
-      });
-    } else {
-      // Browser doesn't support Geolocation
-      handleNoGeolocation(false);
-    }
-  }
-  
-  function handleNoGeolocation(errorFlag) {
-    if (errorFlag) {
-      var content = 'Error: The Geolocation service failed.';
-    } else {
-      var content = 'Error: Your browser doesn\'t support geolocation.';
-    }
-  }
-  
-  google.maps.event.addDomListener(window, 'load', initialize);
+
+function initMap() {
+  // The location of Atlanta
+  var atlanta = { lat: 33.7763658, lng: -84.3899218 };
+  // The map, centered at Atlanta
+  var map = new google.maps.Map(
+    document.getElementById('map'), { zoom: 18, center: atlanta });
+  // The marker, positioned at Atlanta
+  var marker = new google.maps.Marker({ position: atlanta, map: map });
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var queryURL = "http://developer.itsmarta.com/BRDRestService/BRDRestService.svc/";
+
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function (response) {
+  console.log(response);
+});
+
+//http://developer.itsmarta.com/BRDRestService/BRDRestService.svc/GetAllBus
