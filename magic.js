56562976ffbe2a7144628d5bcf3d9a463b808a97
Jesse McKinney <jessman51386@gmail.com>
2020-04-28 16:29:15 -0400

    yay

---
+++ magic.js
@@ -0,0 +1,56 @@
+// var variable = "variable";
+// console.log(variable);
+
+// var strings = ["strings", "booleans", "numbers", "integers", "functions"];
+// var numbers = [1, 2, 3, 4, 5];
+
+// for (i = 0; i < 5; i++) {
+//   console.log("My name is Bob five times");
+// }
+
+// for (i = 0; i < strings.length; i++) {
+//   console.log(strings);
+// }
+
+// var a = 10;
+// var b = 2;
+
+// function divide() {
+//   a / b;
+//   return a / b;
+// }
+// var result = divide(a, b);
+// console.log(result);
+
+// function print() {
+//   for (i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i]);
+//   }
+// }
+// print();
+
+// var object = {
+//   food: "pizza",
+//   animal: "dog",
+//   drink: "monster"
+// };
+
+// console.log(object.food);
+// console.log(object.animal);
+// console.log(object.drink);
+
+// var arrOfObjects = [
+//   (objectsOne = {
+//     food: "pizza",
+//     animal: "dog",
+//     drink: "monster"
+//   }),
+//   (objectsTwo = {
+//       pizza: "food",
+//       dog: "animal",
+//       monster: "drink"
+//   })
+// ];
+
+// console.log(arrOfObjects[1].pizza)
+
